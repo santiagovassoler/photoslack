e0041d5cb1715503fb4f6d24842e8fc3f539cf08
santiago vassoler <santiagovassoler@gmail.com>
2020-04-24 00:40:44 +0100

    add phpdoc

--- app/code/PhotoSlack/Controller/AbstractController.php
+++ app/code/PhotoSlack/Controller/AbstractController.php
@@ -4,6 +4,10 @@ namespace PhotoSlack\Controller;
 
 abstract class AbstractController
 {
+    /**
+     * @param $file file to be rendered
+     * @param array $variables data that will be available through view
+     */
     function render($file, $variables = []) {
         $controllerName = substr(strrchr(get_class($this), "\\"), 1);
         $templateName = ucfirst(str_replace('Controller', '', $controllerName));
