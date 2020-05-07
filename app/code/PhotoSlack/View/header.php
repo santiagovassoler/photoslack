ebbdcda20c033e411286c7e35d3ef2353e25dc45
santiago vassoler <santiagovassoler@gmail.com>
2020-05-07 14:58:37 +0100

    remove google font

--- app/code/PhotoSlack/View/header.php
+++ app/code/PhotoSlack/View/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/pub/static/style/styles.css?<?=filemtime('./pub/static/style/styles.css');?>">
-    <link rel="stylesheet"  type="text/css" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 </head>
 <body>
 <div class="topnav">
