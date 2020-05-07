acdad69bcab5d9f76353e6cdad7e9942ddf03277
santiago vassoler <santiagovassoler@gmail.com>
2020-05-07 15:06:13 +0100

    add phpdocs to get ide support

--- app/code/PhotoSlack/View/Slack/show.php
+++ app/code/PhotoSlack/View/Slack/show.php
@@ -1,30 +1,35 @@
-<?php include 'app/code/PhotoSlack/View/header.php' ?>
 <?php
-/**@var $image */
-/**@var $reaction */
+
+include 'app/code/PhotoSlack/View/header.php';
+
+use PhotoSlack\Model\ReactionModel;
+use PhotoSlack\Model\SlackImage;
+use PhotoSlack\Model\SlackModel;
+
+/**@var SlackModel[] $images */
+/**@var ReactionModel[] $reaction */
+/**@var SlackImage $img */
+
 ?>
+
 <div class="main">
-    <div class="container">
-        <div class="gallery">
-                <div class="<?= count($image) > 1 ? 'main-carousel' : 'no-carousel'?>">
-                    <?php foreach($image as $key => $img): ?>
-                        <div class="carousel-cell">
-                            <?= '<img class="gallery-item" src=' . $img->getImageUrl() . '>'?>
-                        </div>
-                    <?php endforeach;?>
-                </div>
-        </div>
-        <?php foreach ($reaction as $reactions):?>
-        <span class="reactions"><?=$reactions->getName();?><pan class="count"><?=$reactions->getCount();    ?></pan></span>
+    <div class="parent">
+        <?php foreach($images as $image): ?>
+            <?php foreach($image->getImageList() as $img): ?>
+                <?= '<img class="child" src=' .  $img->getImageUrl() . '>'?>
+            <?php endforeach;?>
+        <div>
+            <div class="img-text">
+                <h2><?= $image->getText()?></h2>
+            </div>
         <?php endforeach;?>
+            <div class="reactions">
+                <?php if ($reaction) :?>
+                    <?php foreach ($reaction as $reactions):?>
+                        <span class="reactions-emoji"><?=$reactions->getName();?><pan class="count"><?=$reactions->getCount();?></pan></span>
+                    <?php endforeach;?>
+                <?php endif;?>
+            </div>
+        </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-<script>
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
-    });
-</script>
