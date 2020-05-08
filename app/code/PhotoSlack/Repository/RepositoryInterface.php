8c317f72f4c516a3afe7aa93ff2310dcda105cc0
santiago vassoler <santiagovassoler@gmail.com>
2020-05-08 04:55:30 +0100

    extract methods to a abstract class

--- app/code/PhotoSlack/Repository/RepositoryInterface.php
+++ app/code/PhotoSlack/Repository/RepositoryInterface.php
@@ -8,12 +8,4 @@ interface RepositoryInterface
     public function getCollection() : array;
 
     public function show($ts) : array;
-
-    public function getDataAsArray(string $apiUrl, string $apiToken, array $methodArray, string $method, array $argument) : array;
-
-    public function getRequest(array $request) : string;
-
-    public function getPublicSecret(string $key): string;
-
-    public function queryAPI(string $method, array $params): array;
 }
